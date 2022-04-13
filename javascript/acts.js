82feb0902df10155b4b353070805226c96cdd6e3
shaqdeff <shaquillendunda@gmail.com>
2022-04-13 07:45:24 +0300

    Create array of objects for cards

---
+++ javascript/acts.js
@@ -0,0 +1,48 @@
+const actsContent = [
+ {
+  image: "images/photo1.jpeg",
+  icon: "images/music.png",
+  title: "Black Tea",
+  subHeading: "DJ, Record Producer & Songwriter",
+  description: `Black Tea is a South African DJ, record producer, and
+                songwriter. He's one of the most influential acts on the
+                continent known exclusively for his soulful Afro-House sound.
+                Tea has established his own record label, Soulistic Music, and
+                has recently released a self-titled album called Black Tea.`,
+ },
+ {
+  image: "images/photo2.jpeg",
+  icon: "images/music.png",
+  title: "Wiz",
+  subHeading: "Singer & Songwriter",
+  description: `Wiz is a Nigerian singer and songwriter. He has multiple awards including a grammy and is considered one of the greatest African artists. By February 2021, Wiz was the most-streamed Nigerian artist of all time on Spotify, with over 3.4 billion streams across all credits.`,
+ },
+ {
+  image: "images/photo3.jpeg",
+  icon: "images/music.png",
+  title: "Wendy Kay",
+  subHeading: "Singer, Songwriter & Producer",
+  description: `Wendy Kay is an Afro-Soul artist based in Nairobi, Kenya. She describes herself as an ever-growing evolving being, a creator, and a lover of arts. Her music exudes a relaxed mix of Afro-Soul and Afro Pop.`,
+ },
+ {
+  image: "images/photo4.jpeg",
+  icon: "images/music.png",
+  title: "Funk Avenue",
+  subHeading: "Rap Duo & Songwriters",
+  description: `Funk Avenue is a rap music duo based in Nairobi, Kenya. They are two rappers, songwriters, and entrepreneurs deeply rooted in the Kenyan hip-hop scene. They are regarded as one of the most versatile and unique artists to grace the Kenyan music industry.`,
+ },
+ {
+  image: "images/photo5.jpeg",
+  icon: "images/music.png",
+  title: "GGTheAscendant",
+  subHeading: "Singer & Songwriter",
+  description: `GGTheAscendant is a Kenyan Afro-Fusion musician. She brings an extensive mastery of lyricism and an innate sense of rhythm to her music. She views her music as art bursting forth with relentless energetic expression.`,
+ },
+ {
+  image: "images/photo6.jpeg",
+  icon: "images/music.png",
+  title: "Zonda Kay",
+  subHeading: "Singer, Songwriter & Guitarist",
+  description: `Zonda Kay is a Kenyan Afro-Fusion artist. His music is inspired by African culture and personal experiences. His goal is to ignite the souls of Africa and beyond with his sound and vocal prowess.`,
+ },
+];
